5d12763d13d3b67d0e690394cb4affd42057310b
Jameka Echols <jameka.echols@gmail.com>
2021-05-29 22:27:33 -0500

    added uiimageviewcontrollerdelegate protocols implemented

--- MemeMe1.0/ViewController.swift
+++ MemeMe1.0/ViewController.swift
@@ -7,21 +7,43 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var bottomTextfield: UITextField!
     @IBOutlet weak var topTextfield: UITextField!
     @IBOutlet weak var cameraButton: UIToolbar!
-    @IBOutlet weak var albumButton: UIBarButtonItem!
     @IBOutlet weak var uploadButton: UIBarButtonItem!
     @IBOutlet weak var cancelButton: UIBarButtonItem!
-    
+    @IBOutlet weak var imageViewPicker: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
     }
 
+    // Mark: ACTIONs for choosing an image or taking one
 
+    @IBAction func pickAnImage(_ sender: Any) {
+        let pickImageController = UIImagePickerController()
+        pickImageController.delegate = self
+        present(pickImageController, animated: true, completion: nil)
+    }
+    
+    
+    
+    // Mark: protocols
+    
+    // save the chosen image to the imageViewPicker
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let pic = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
+        
+        imageViewPicker.image = pic
+    }
+    
+    
+    // now dismiss the picker
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
 }
 
