2780f32452dc76e40eb86dae94c5504e0988e113
Jameka Echols <jameka.echols@gmail.com>
2021-05-31 13:44:33 -0500

    added protocols for the text field and added the attributes

--- MemeMe1.0/ViewController.swift
+++ MemeMe1.0/ViewController.swift
@@ -7,22 +7,45 @@
 
 import UIKit
 
-class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextFieldDelegate{
 
     @IBOutlet weak var bottomTextfield: UITextField!
     @IBOutlet weak var topTextfield: UITextField!
     @IBOutlet weak var cameraButton: UIToolbar!
+    @IBOutlet weak var albumButton: UIBarButtonItem!
     @IBOutlet weak var uploadButton: UIBarButtonItem!
     @IBOutlet weak var cancelButton: UIBarButtonItem!
     @IBOutlet weak var imageViewPicker: UIImageView!
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        //disable the upload button
+        uploadButton.isEnabled = false;
+        
+        //set the text attributes for top and bottom
+        let memeTextAttributes: [NSAttributedString.Key: Any] = [
+            NSAttributedString.Key.strokeColor: UIColor.black,
+            NSAttributedString.Key.foregroundColor: UIColor.white,
+            NSAttributedString.Key.font: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
+            NSAttributedString.Key.strokeWidth:  1.2
+        ]
+        
+        topTextfield.defaultTextAttributes = memeTextAttributes
+        bottomTextfield.defaultTextAttributes = memeTextAttributes
+        topTextfield.textAlignment = .center
+        bottomTextfield.textAlignment = .center
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        // enable the upload button once the top and bottom text have values and the imageviewpicker has an image
+        if bottomTextfield.text != nil && topTextfield.text != nil && imageViewPicker.image != nil {
+            uploadButton.isEnabled = true
+        }
     }
 
-    // Mark: ACTIONs for choosing an image or taking one
-
+    // MARK: ACTIONs for choosing an image or taking one
     @IBAction func pickAnImage(_ sender: Any) {
         let pickImageController = UIImagePickerController()
         pickImageController.delegate = self
@@ -30,10 +53,15 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         present(pickImageController, animated: true, completion: nil)
     }
     
+    @IBAction func pickAnImageFromCamera(_ sender: Any) {
+        let cameraController = UIImagePickerController()
+        cameraController.delegate = self
+        cameraController.sourceType = .camera
+        present(cameraController, animated: true, completion: nil)
+    }
     
     
-    // Mark: protocols
-    
+    // MARK: Protocols for image picker
     // save the chosen image to the imageViewPicker
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         let pic = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
@@ -41,10 +69,18 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         imageViewPicker.image = pic
     }
     
-    
     // now dismiss the picker
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
         dismiss(animated: true, completion: nil)
+    }
+    
+    // MARK : Protocols for textfield
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        textField.clearsOnBeginEditing = true
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        return true
     }
 }
 
