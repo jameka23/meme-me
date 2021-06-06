18a606b7c55c2d373cdee740b713feea9123c145
Jameka Echols <jameka.echols@gmail.com>
2021-06-06 00:05:42 -0500

    auto layout needs to rethink its approach

--- MemeMe1.0/MemeEditorViewController.swift
+++ MemeMe1.0/MemeEditorViewController.swift
@@ -80,7 +80,7 @@ class MemeEditorViewController: UIViewController, UIImagePickerControllerDelegat
         let memeTextAttributes: [NSAttributedString.Key: Any] = [
             NSAttributedString.Key.strokeColor: UIColor.black,
             NSAttributedString.Key.foregroundColor: UIColor.white,
-            NSAttributedString.Key.font: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
+            NSAttributedString.Key.font: UIFont(name: "HelveticaNeue-CondensedBlack", size: 30)!,
             NSAttributedString.Key.strokeWidth:  -5.0
         ]
         
